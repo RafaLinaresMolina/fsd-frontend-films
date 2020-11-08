b419005b5e911a1a350a7362c2e14c049dcc952b
Rafa Linares <rafa.linares.molina@gmail.com>
2020-11-08 19:46:34 +0100

    Modified modal component
    the modal now can be closed by hitting Esc key, and accept a theme as prop. Also I added some themes

---
+++ src/themes/themes.js
@@ -0,0 +1,45 @@
+export const greenTheme = {
+  color: {
+    primary: {
+      base: '#75de50',
+      dark: '#4cb827',
+      light: '#aaff8c'
+    },
+    header: {
+      base: '#75de50',
+      dark: '#4cb827',
+      light: '#aaff8c'
+    },
+  }
+};
+
+export const orangeTheme = {
+  color: {
+    primary: {
+      base: '#ffc124',
+      dark: '#e3a914',
+      light: '#ffde8a'
+    },
+    header: {
+      base: '#ffc124',
+      dark: '#e3a914',
+      light: '#ffde8a'
+    },
+  }
+};
+
+
+export const redTheme = {
+  color: {
+    primary: {
+      base: '#ff1f1f',
+      dark: '#e31414',
+      light: '#ff6e6e'
+    },
+    header: {
+      base: '#ff1f1f',
+      dark: '#e31414',
+      light: '#ff6e6e'
+    },
+  }
+};
