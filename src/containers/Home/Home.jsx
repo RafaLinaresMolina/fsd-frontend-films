f63dc168bdf11b7480b2749890e136d914c90c3e
Agredas <andreagredas@gmail.com>
2020-11-14 16:35:47 +0100

    Home Page finished.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Carousel } from 'antd';
-import { Button } from "arwes";
+import { Button, Puffs } from "arwes";
 import './Home.scss';
 import React, {useState} from 'react';
 import Modal from '../../components/Modal/Modal';
@@ -25,30 +25,53 @@ const Home = () => {
 
 return(
   <div>
-  <div className="imgCarrusel">
-    <Carousel dots={false} autoplay={onChange}>
-    <div class="imagen1">
+    <div className="imgCarrusel">
+      <Carousel dots={false} autoplay={onChange}>
+        <div class="imagen1">
+        </div>
+        <div class="imagen2">
+        </div>
+        <div class="imagen3">
+        </div>
+        <div class="imagen4">
+        </div>
+      </Carousel>
     </div>
-    <div class="imagen2">
+    
+    <div className="header">
+      <div className='logo'>GEEKFLIX</div>
     </div>
-    <div class="imagen3">
-    </div>
-    <div class="imagen4">
-    </div>
-  </Carousel>
-  </div>
-    <div className="modals">
-    <Button animate onClick={() => {showModalLogin()}}>Login</Button>
-    <Modal show={showLogin} handleClose={hideModalLogin} title={'Login'}>
-      <Login />
-    </Modal>
 
-    <Button animate onClick={() => {showModalRegister()}}>Register </Button>
-    <Modal show={showRegister} handleClose={hideModalRegister} title={'Register'}>
-      <Register />
-    </Modal>
+    <div className="buttons">
+      <div>
+        <Button animate onClick={() => {showModalLogin()}}>Login</Button>
+        <Modal show={showLogin} handleClose={hideModalLogin} title={'Login'}>
+          <Login />
+        </Modal>
+      </div>
+      <div className="hole"></div>
+      <div>
+        <Button animate onClick={() => {showModalRegister()}}>Register </Button>
+        <Modal show={showRegister} handleClose={hideModalRegister} title={'Register'}>
+          <Register />
+        </Modal>
+      </div>
     </div>
+
+    <div className="phrase">Your best movie rental site, for geeks like you!</div>
+
+    <Puffs>
+        <div style={{ width: '90%', height: 40 }} />
+    </Puffs>
+    <Puffs>
+        <div style={{ width: '50%', height: 30 }} />
+    </Puffs>
+    <Puffs>
+        <div style={{ width: '20%', height: 20 }} />
+    </Puffs>
+
   </div>
+
  );
 }
 
