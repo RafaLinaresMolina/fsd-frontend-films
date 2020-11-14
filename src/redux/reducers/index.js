564821d2b846302d9f0aac97cdc1f345bba9ac85
robergeekhub <robergeekhub@gmail.com>
2020-11-14 10:52:05 +0100

    added profile.jsx and Profile.scss

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,13 @@ import film from './film';
 import user from './user';
 import admin from './admin';
 import notification from './notification';
+import profile from './profile';
+
 const reducer = combineReducers({
     filmReducer: film,
     userReducer: user,
     notificationReducer: notification,
-    adminReducer: admin
+    adminReducer: admin,
+    profileReducer: profile
 })
 export default reducer;
