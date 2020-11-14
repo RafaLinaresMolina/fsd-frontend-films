d74d6e389acd147fc8f47129d4fd9deb6e8f80e5
Agredas <andreagredas@gmail.com>
2020-11-14 09:55:39 +0100

    Login and Register Buttons created.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,19 +1,30 @@
 import { Carousel } from 'antd';
+import { Button } from "arwes";
 import './Home.scss';
+import React, {useState} from 'react';
+import Modal from '../../components/Modal/Modal';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+
 
 
 const Home = () => {
   function onChange(a) {
     console.log(a);
   }
-const contentStyle = {
-  height: '100%',
-  color: '#fff',
-  lineHeight: '43.6em',
-  textAlign: 'center',
-  };
+
+  
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const showModalRegister = () => {setShowRegister(true);};
+  const hideModalRegister = () => {setShowRegister(false);};
+
+  const showModalLogin = () => {setShowLogin(true);};
+  const hideModalLogin = () => {setShowLogin(false);};
 
 return(
+  <div>
   <div className="imgCarrusel">
     <Carousel dots={false} autoplay={onChange}>
     <div class="imagen1">
@@ -26,7 +37,18 @@ return(
     </div>
   </Carousel>
   </div>
-  
+    <div className="modals">
+    <Button animate onClick={() => {showModalLogin()}}>Login</Button>
+    <Modal show={showLogin} handleClose={hideModalLogin} title={'Login'}>
+      <Login />
+    </Modal>
+
+    <Button animate onClick={() => {showModalRegister()}}>Register </Button>
+    <Modal show={showRegister} handleClose={hideModalRegister} title={'Register'}>
+      <Register />
+    </Modal>
+    </div>
+  </div>
  );
 }
 
