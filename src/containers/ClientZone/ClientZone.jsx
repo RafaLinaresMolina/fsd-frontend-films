6f59cccf0e774da5371ef3228edb0170f4406445
Rafa Linares <rafa.linares.molina@gmail.com>
2020-11-15 17:15:13 +0100

    Added functionals catalogs on app
    modified with redux to ask for films, also fixed film action

--- src/containers/ClientZone/ClientZone.jsx
+++ src/containers/ClientZone/ClientZone.jsx
@@ -3,7 +3,24 @@ import Catalog from "../../components/Catalog/Catalog";
 import { Content, Header } from "arwes";
 import Footer from "../../components/Footer/Footer";
 import "./ClientZone.scss";
+import { useEffect } from "react";
+import { getAllFilms } from "../../redux/actions/film";
+import { ERROR_NOTIFICATION } from "../../redux/types/notificationTypes";
 function ClientZone(props) {
+
+  useEffect(() => {
+    getAllFilms().catch(err => props.dispatch({
+      type: ERROR_NOTIFICATION,
+      payload: {
+        notification: {
+          title: "ERROR RETRIVING FILMS!",
+          msg: err.message,
+        },
+        show:true
+      },
+    }))
+  }, [])
+
   return (
     <div className="clientZone">
       <Content>
@@ -16,10 +33,10 @@ function ClientZone(props) {
         </div>
         
         <div className="contentClient">
-          <Catalog title={"Search result "} content={props} showAllways={false} />
-          <Catalog title={`Actor: ${""} `} content={props} showAllways={false} />
-          <Catalog title={`Genre: ${""} `} content={props} showAllways={false} />
-          <Catalog title={"All Films"} content={props} />
+          <Catalog title={"Search result "} content={props.filmsByTitle} showAllways={false} />
+          <Catalog title={`Actor: ${""} `} content={props.filmsByActor} showAllways={false} />
+          <Catalog title={`Genre: ${""} `} content={props.filmsByGenre} showAllways={false} />
+          <Catalog title={"All Films"} content={props.films} showAllways={true}/>
         </div>
 
         <Footer />
@@ -31,6 +48,10 @@ function ClientZone(props) {
 const mapStateToProps = (state) => {
   return {
     user: state.userReducer.user,
+    films: state.filmReducer.films,
+    filmsByTitle: state.filmReducer.filmsByTitle,
+    filmsByActor: state.filmReducer.filmsByActor,
+    filmsByGenre: state.filmReducer.filmsByGenre,
   };
 };
 
