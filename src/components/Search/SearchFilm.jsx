9f65696323a4bdda8a9d3d9a4a88bf47f2204710
Agredas <andreagredas@gmail.com>
2020-11-16 02:06:45 +0100

    Search by Genre or Actor.

--- src/components/Search/SearchFilm.jsx
+++ src/components/Search/SearchFilm.jsx
@@ -1,57 +1,55 @@
-import React, { Component, useEffect, useState } from 'react';
-import FilmList from '../FilmList/FilmList';
-import {useHistory} from 'react-router-dom';
-import {Button} from 'arwes';
-import axios from 'axios';
-import { getFilmsByName } from '../../redux/actions/film';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'arwes';
+import { getFilmsByName, getFilmsByActor, getFilmsByGenre } from '../../redux/actions/film';
 import './SearchFilm.scss';
 import MagnifyIcon from 'mdi-react/MagnifyIcon';
 
 
-const Search = (props) =>{
-  
+const Search = (props) => {
+
   useEffect(() => {
     try {
-     
-      
+
+
     } catch (err) {
       console.log(err);
     }
   }, []);
-  const [search, setSearch]= useState("");
-  
+  const [search, setSearch] = useState("");
 
-  const eventHandler = (ev) =>{
+
+  const eventHandler = (ev) => {
     setSearch(ev.target.value)
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-try { await getFilmsByName(search)    
-  
-} catch (error) {
-  console.log(error.message)
-}
-}
+    try {
+      await getFilmsByName(search)
 
-
-      return(
-            <form onSubmit={handleSubmit}>
-                <div className="field-search">
-                    <div className="control">
-                        <input 
-                        className="inputSearch"
-                        onChange={eventHandler}
-                        type="text" 
-                        />
-                    </div>
-                    <div className="control">
-                  <Button className="button is-info" >
-                  <MagnifyIcon className='verticalAlignIcons'/>Search
-                        </Button>
-                    </div>
-                </div>
-            </form>
-        )
+    } catch (error) {
+      console.log(error.message)
     }
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field-search">
+        <div className="control">
+          <input
+            className="inputSearch"
+            onChange={eventHandler}
+            type="text"
+          />
+        </div>
+        <div className="control">
+          <Button className="button is-info" >
+            <MagnifyIcon className='verticalAlignIcons' />Search
+                        </Button>
+        </div>
+      </div>
+    </form>
+  )
+}
 
 export default Search;
